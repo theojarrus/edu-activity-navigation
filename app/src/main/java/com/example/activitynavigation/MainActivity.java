4184ae266107db7e5e0a34f626f3c4aecaee8f36
f.sedov <f.sedov@tinkoff.ru>
2024-01-15 19:37:13 +0300

    Add system navigation

--- app/src/main/java/com/example/activitynavigation/MainActivity.java
+++ app/src/main/java/com/example/activitynavigation/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.activitynavigation;
 
+import static android.content.Intent.ACTION_GET_CONTENT;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.activitynavigation.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,16 +36,35 @@ public class MainActivity extends AppCompatActivity {
             }
     );
 
+    private final ActivityResultLauncher<String> filesResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.GetContent(),
+            result -> showSnackBar(result.getPath())
+    );
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater(), null, false);
         setContentView(binding.getRoot());
 
-        binding.open.setOnClickListener(v -> {
+        binding.openActivity.setOnClickListener(v -> {
             String text = binding.field.getText().toString();
             Intent intent = ExampleActivity.createIntent(this, text, RESULT_TEXT_KEY);
             activityResultLauncher.launch(intent);
+        });
+
+        binding.openCamera.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivity(intent);
+        });
+
+        binding.chooseFile.setOnClickListener(v -> {
+            // Intent intent = new Intent();
+            // intent.setAction(Intent.ACTION_GET_CONTENT);
+            // intent.setType("*/*");
+            // startActivity(intent);
+            filesResultLauncher.launch("*/*");
         });
     }
 
@@ -54,5 +80,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void showSnackBar(String message) {
+        Snackbar.make(this, binding.getRoot(), message, Snackbar.LENGTH_SHORT).show();
     }
 }
